374c36d175fb3f8041e5a421c2a48329c0dd9acc
Chase Roth <cxr6988@g.rit.edu>
2021-05-05 20:03:40 -0400

    + equality division

---
+++ ImageDividerTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace ImageDividerTest
+{
+    public class UnitTest1
+    {       
+        public void Test1()
+        {
+
+        }
+
+        [Theory(DisplayName = "")]
+        [InlineData(1)]
+        public void Test2(int example) { }
+    }
+}
