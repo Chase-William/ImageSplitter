f82f509cc7cbc942353d0aff9fd2e2b967855697
Chase Roth <cxr6988@g.rit.edu>
2021-05-06 21:27:20 -0400

    + sample  proj

---
+++ Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using ImageSplitter;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the ImageSplitter Sample!");
+            Console.WriteLine("The image that is being split is called nature.bmp and it resides in a <resource> folder in this repository's root directory.");
+
+            using var splitter = new ImageGrid("../../../../resources/nature.bmp", 4, 4);            
+            splitter.SaveBitmaps("example", SkiaSharp.SKEncodedImageFormat.Png, 55);
+
+            Console.WriteLine("Check your bin/** folder for the output.");
+            Console.ReadLine();
+        }
+    }
+}
